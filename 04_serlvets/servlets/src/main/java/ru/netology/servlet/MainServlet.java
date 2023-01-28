b17cfe392da32707dfee7740a27a96607d38d17b
Veryval <veryval@mail.ru>
2023-01-28 15:11:52 +0300

    DI Java Configuration

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -2,6 +2,7 @@ package ru.netology.servlet;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
+import ru.netology.javaconfig.JavaConfig;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
@@ -18,7 +19,7 @@ public class MainServlet extends HttpServlet {
         //final var repository = new PostRepository();
         //final var service = new PostService(repository);
         //controller = new PostController(service);
-        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
         controller = (PostController) context.getBean("postController");
     }
 
